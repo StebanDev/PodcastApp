2541d329552db59c08dddc086c7dd9d6c5b7f5ec
Esteban C <eciudadconde@gmail.com>
2019-05-29 01:32:13 -0500

    Vista de Podcast

--- pages/channel.js
+++ pages/channel.js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default class extends React.Component {
   static async getInitialProps({ query }) {
     const id = query.id;
@@ -32,13 +34,25 @@ export default class extends React.Component {
         <h1>{channel.title}</h1>
 
         <h2>Series</h2>
-        {series.map(serie => (
-          <div>{serie.title}</div>
-        ))}
+        <div className="channels">
+          {series.map(serie => (
+            <div className="channel">
+              <img src={serie.urls.logo_image.original} alt="" />
+              <h3>{serie.title}</h3>
+            </div>
+          ))}
+        </div>
 
         <h2>Últimos PodCasts</h2>
         {audioClips.map(clip => (
-          <div>{clip.title}</div>
+          <Link href={`/podcast?id=${clip.id}`}>
+            <a className="podcast">
+              <h3>{clip.title}</h3>
+              <div className="meta">
+                {Math.ceil(clip.duration / 60)} minutes
+              </div>
+            </a>
+          </Link>
         ))}
 
         <style jsx>
@@ -73,6 +87,25 @@ export default class extends React.Component {
               font-weight: 600;
               margin: 0;
               text-align: center;
+            }
+            .podcast {
+              display: block;
+              text-decoration: none;
+              color: #333;
+              padding: 15px;
+              border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+              cursor: pointer;
+            }
+            .podcast:hover {
+              color: #000;
+            }
+            .podcast h3 {
+              margin: 0;
+            }
+            .podcast .meta {
+              color: #666;
+              margin-top: 0.5em;
+              font-size: 0.8em;
             }
           `}
         </style>
