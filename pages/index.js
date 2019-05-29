bafd319b2ccaeaea15d8a739d5fe854dda6dad5d
Esteban C <eciudadconde@gmail.com>
2019-05-29 00:39:30 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+export default class extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hola Platzi</h1>
+
+        <style jsx global>
+          {`
+            body {
+              margin: 0;
+              font-family: Arial;
+              background-color: white;
+            }
+          `}
+        </style>
+      </div>
+    );
+  }
+}
