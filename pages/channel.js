a980488f03e2038a061558198be53909cc819bd9
Esteban C <eciudadconde@gmail.com>
2019-05-29 22:23:21 -0500

    Personalizando errores

--- pages/channel.js
+++ pages/channel.js
@@ -1,7 +1,7 @@
 import ChannelGrid from '../components/ChannelGrid';
 import Layout from '../components/Layout';
 import PodcastList from '../components/PodcastList';
-import Error from 'next/error';
+import Error from './_error';
 
 export default class extends React.Component {
   static async getInitialProps({ query, res }) {
